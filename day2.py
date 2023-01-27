09713886d3a7dbda17321f105281b08ce8ac8931
235909 <245826@edu.p.lodz.pl>
2023-01-27 21:52:37 +0100

    Advent of Code 2022 - Python Solutions

--- day2.py
+++ day2.py
@@ -34,7 +34,38 @@ def part1():
 
 
 def part2():
-    pass
+    rock_points = 1
+    paper_points = 2
+    scissors_points = 3
+    win_points = 6
+    draw_points = 3
+    # lose points -> 0
+    my_points = 0
+    file = open("day2.txt", "r")
+    for line in file:
+        if line[0] == "A":
+            if line[2] == "X":
+                my_points +=scissors_points
+            elif line[2] == "Y":
+                my_points += draw_points+rock_points
+            elif line[2] == "Z":
+                my_points += win_points+paper_points
+        elif line[0] == "B":
+            if line[2] == "X":
+                my_points +=rock_points
+            elif line[2] == "Y":
+                my_points += draw_points+paper_points
+            elif line[2] == "Z":
+                my_points += win_points+scissors_points
+        elif line[0] == "C":
+            if line[2] == "X":
+                my_points +=paper_points
+            elif line[2] == "Y":
+                my_points += draw_points+scissors_points
+            elif line[2] == "Z":
+                my_points += win_points+rock_points
+    file.close()
+    return my_points
 
 
 if __name__ == "__main__":
