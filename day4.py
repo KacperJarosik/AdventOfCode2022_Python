74a9aa1850be3dbe3cdd3b1d109e6b5226597119
235909 <245826@edu.p.lodz.pl>
2023-01-31 18:15:23 +0100

    add part 2 day4

--- day4.py
+++ day4.py
@@ -18,10 +18,21 @@ def part1():
 
 
 def part2():
+    account_compartment_within_the_compartment = 0
     file = open("input_day4.txt", "r")
-
+    for line in file:
+        if line[-1] == "\n":
+            line = line[:-1]
+        line = line.replace(",", "-")
+        line = line.split("-")
+        first_down = int(line[0])  # first1
+        first_up = int(line[1])  # second1
+        second_down = int(line[2])  # first2
+        second_up = int(line[3])  # second2
+        if not (first_down > second_up or first_up < second_down or second_down > first_up or second_up < first_down):
+            account_compartment_within_the_compartment += 1
     file.close()
-    return
+    return account_compartment_within_the_compartment
 
 
 if __name__ == "__main__":
